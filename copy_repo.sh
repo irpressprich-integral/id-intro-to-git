20b54fca2962cdbeff68e5b7f821b5eb2ac909fd
Robert Pressprich <robert.pressprich@autonomic.ai>
2022-03-08 11:01:04 -0500

    fix error in shell script

--- copy_repo.sh
+++ copy_repo.sh
@@ -1,9 +1,9 @@
-if ([[ -z $1]])
+if ([[ -z $1 ]])
 then
 	echo "Please enter a git repo url"
 else
 	git branch -r | grep -v '\->' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
 	git pull --all
-	git set-url origin $1
+	git remote set-url origin $1
 	git push --all
 fi
