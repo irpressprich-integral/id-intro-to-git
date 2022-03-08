8486ea60426b8abf743063100257f166aceb5a05
Robert Pressprich <55556127+irpressprich-integral@users.noreply.github.com>
2022-03-08 10:00:13 -0500

    Create animals.py

---
+++ animals.py
@@ -0,0 +1,9 @@
+animal = input("What animal are you?: ")
+
+if animal == "":
+   print("No input! Exiting...")
+   exit()
+
+print(f"What does the {animal} say?")
+
+print("I don't know that one...")
